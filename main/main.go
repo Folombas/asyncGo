9a10ff723f86e2ba033b72363c9958f8ff98c539
Folombas <folombas@yandex.ru>
2025-07-17 09:01:37 +0300

    канал отмены

--- main/main.go
+++ main/main.go
@@ -1,24 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	ch1 := make(chan int, 2)
-	ch1 <- 1
-	ch1 <- 2
-	ch2 := make(chan int, 2)
-	ch2 <- 3
-LOOP:
-	for {
-		select {
-		case v1 := <-ch1:
-			fmt.Println("chan1 val", v1)
-		case v2 := <-ch2:
-			fmt.Println("chan2 val", v2)
-		default:
-			break LOOP
+	cancelCh := make(chan struct{})
+	dataCh := make(chan int)
+
+	go func(cancelCh chan struct{}, dataCh chan int) {
+		val := 0
+		for {
+			select {
+			case <-cancelCh:
+				return
+			case dataCh <- val:
+				val++
+			}
 		}
-	}
+	}(cancelCh, dataCh)
 }
