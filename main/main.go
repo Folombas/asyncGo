5c5ef60a0691b7678c9254a7b3ec8a9b52219816
Folombas <folombas@yandex.ru>
2025-07-17 08:58:03 +0300

    LOOP chan

--- main/main.go
+++ main/main.go
@@ -5,15 +5,20 @@ import (
 )
 
 func main() {
-	ch1 := make(chan int, 1)
-	ch2 := make(chan int)
+	ch1 := make(chan int, 2)
 	ch1 <- 1
-	select {
-	case val := <-ch1:
-		fmt.Println("ch1 val", val)
-	case ch2 <- 1:
-		fmt.Println("put val to ch2")
-	default:
-		fmt.Println("default case")
+	ch1 <- 2
+	ch2 := make(chan int, 2)
+	ch2 <- 3
+LOOP:
+	for {
+		select {
+		case v1 := <-ch1:
+			fmt.Println("chan1 val", v1)
+		case v2 := <-ch2:
+			fmt.Println("chan2 val", v2)
+		default:
+			break LOOP
+		}
 	}
 }
