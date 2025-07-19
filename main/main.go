8cd7dad4d18e12612885daedbd46f810ba336c60
Folombas <folombas@yandex.ru>
2025-07-19 11:25:22 +0300

    function AfterFunc

--- main/main.go
+++ main/main.go
@@ -5,21 +5,15 @@ import (
 	"time"
 )
 
-func main() {
-	ticker := time.NewTicker(time.Second)
-	i := 0
-	for tickTime := range ticker.C {
-		i++
-		fmt.Println("step: ", i, "time: ", tickTime)
-		if i >= 5 {
-			// надо останавливать, иначе потечёт
-			ticker.Stop()
-			break
-		}
-	}
-	fmt.Println("total", i)
-
-
-	return
+func sayHello() {
+	fmt.Println("Hello, Go Programming Language!")
 }
 
+func main() {
+	timer := time.AfterFunc(1 * time.Second, sayHello)
+
+	fmt.Scanln()
+	timer.Stop()
+
+	fmt.Scanln()
+}
