9b5a37e5356445d1ca81594b5c57f6fe21cda002
Folombas <folombas@yandex.ru>
2025-07-21 20:57:09 +0300

    rate limiting in Go

--- main/main.go
+++ main/main.go
@@ -1,46 +1,59 @@
 package main
 
-// Пример ratelim
-
 import (
 	"fmt"
-	"runtime"
-	"strings"
 	"sync"
 	"time"
 )
 
-const (
-	iterationsNum = 6
-	goroutinesNum = 5
-	quotaLimit    = 2
-)
+// RateLimiter - простой ограничитель частоты
+type RateLimiter struct {
+	interval time.Duration
+	maxCount int
+	count    int
+	mu       sync.Mutex
+}
 
-func startWorker(in int, wg *sync.WaitGroup, quotaCh chan struct{}) 
-	{
-		quotaCh <- struct{}{} // ratelim.go, берём свободный слот
-		defer wg.Done()
-		for j := 0; j < iterationsNum; j++ {
-			fmt.Printf(formatWork(in, j))
-
-			// if j%2 == 0 {
-			// <-quotaCh               // ratelim.go, возвращаем слот
-			// quotaCh <- struct{}{}   // ratelim.go, берём свободный слот
-			//}
-
-			runtime.Gosched()   // Даём поработать другим горутинам
-		}
-		<-quotaCh // ratelim.go, возвращаем слот
+func NewRateLimiter(interval time.Duration, maxCount int) *RateLimiter {
+	rl := &RateLimiter{
+		interval: interval,
+		maxCount: maxCount,
 	}
+
+	// Автосброс счетчика
+	go func() {
+		for {
+			time.Sleep(rl.interval)
+			rl.mu.Lock()
+			rl.count = 0
+			rl.mu.Unlock()
+		}
+	}()
+
+	return rl
+}
+
+func (rl *RateLimiter) Allow() bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if rl.count < rl.maxCount {
+		rl.count++
+		return true
+	}
+	return false
+}
 
 func main() {
-	wg := &sync.WaitGroup{}
-	quotaCh := make(chan struct{}, quotaLimit)  // ratelim.go
-	for i := 0; i < goroutinesNum; i++ {
-		wg.Add(1)
-		go startWorker(i, wg, quotaCh)
+	// 5 операций в секунду
+	limiter := NewRateLimiter(time.Second, 5)
+
+	for i := 1; i <= 10; i++ {
+		if limiter.Allow() {
+			fmt.Printf("%s: Операция %d - разрешена\n", time.Now().Format("15:04:05"), i)
+		} else {
+			fmt.Printf("%s: Операция %d - ограничена!\n", time.Now().Format("15:04:05"), i)
+		}
+		time.Sleep(200 * time.Millisecond) // Искусственная задержка
 	}
-	time.Sleep(time.Microsecond)
-	wg.Wait()
-	
 }
