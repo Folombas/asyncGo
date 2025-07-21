870cc5722397cd3dc27312d246ddb97e3567e872
Folombas <folombas@yandex.ru>
2025-07-21 20:32:17 +0300

    что такое ratelim

--- main/main.go
+++ main/main.go
@@ -1,21 +1,46 @@
 package main
 
+// Пример ratelim
+
 import (
 	"fmt"
+	"runtime"
+	"strings"
 	"sync"
+	"time"
 )
 
-func main() {
-	var wg sync.WaitGroup
+const (
+	iterationsNum = 6
+	goroutinesNum = 5
+	quotaLimit    = 2
+)
 
-	for i := 0; i < 3; i++ {
-		wg.Add(1) // +1 к счетчику
-		go func(id int) {
-			defer wg.Done() // Гарантированный -1 при выходе
-			fmt.Printf("Горутина %d завершена\n", id)
-		}(i)
+func startWorker(in int, wg *sync.WaitGroup, quotaCh chan struct{}) 
+	{
+		quotaCh <- struct{}{} // ratelim.go, берём свободный слот
+		defer wg.Done()
+		for j := 0; j < iterationsNum; j++ {
+			fmt.Printf(formatWork(in, j))
+
+			// if j%2 == 0 {
+			// <-quotaCh               // ratelim.go, возвращаем слот
+			// quotaCh <- struct{}{}   // ratelim.go, берём свободный слот
+			//}
+
+			runtime.Gosched()   // Даём поработать другим горутинам
+		}
+		<-quotaCh // ratelim.go, возвращаем слот
 	}
 
-	wg.Wait() // Ждём обнуления счетчика
-	fmt.Println("Все горутины завершились")
+func main() {
+	wg := &sync.WaitGroup{}
+	quotaCh := make(chan struct{}, quotaLimit)  // ratelim.go
+	for i := 0; i < goroutinesNum; i++ {
+		wg.Add(1)
+		go startWorker(i, wg, quotaCh)
+	}
+	time.Sleep(time.Microsecond)
+	wg.Wait()
+	
 }
